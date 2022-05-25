ab1e5c7d953759ed9b3bcde75d4b7e18235efa80
redgun123 <red_gun@naver.com>
2022-05-25 12:32:52 +0900

    자바 설정

---
+++ src/main/java/me/Lja/config/WebConfig.java
@@ -0,0 +1,31 @@
+package me.Lja.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+	protected Filter[] gertServletFilter() {
+		CharacterEncodingFilter eFilter = new CharacterEncodingFilter();
+		eFilter.setEncoding("utf-8");
+		eFilter.setForceEncoding(true);
+		return new Filter[] {eFilter};
+	}
+}
